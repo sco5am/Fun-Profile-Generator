cb7a1ed1f7e9183629d776c348b8bdadff0e99c7
Sam Oaks <samuelclayoaks@yahoo.com>
2023-01-08 17:31:43 -0500

    :added employee code

---
+++ tests/Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require('../lib/Employee');
+const teamMember = new Employee('Samuel', '4201997', 'sco5am@yahoo.com');
+
+Test('test if the employee object can get the constructor values', () => {
+    expect(teamMember.name).toBe('Samuel');
+    expect(teamMember.id).toBe('4201997');
+    expect(teamMember.email).toBe('sco5am@yahoo.com');
+});
+
+test('will get the name from the getName()method', () => {
+    expect(teamMember.getName()).toBe('Samuel');
+});
+
+test('will get the id from the getId() method', () => {
+    expect(teamMember.getId()).toBe('4201997');
+});
+
+test('will get the email from the getEmail() method', () => {
+    expect(teamMember.getEmail()).toBe('sco5am@yahoo.com');
+});
+
+test('will get the member from the getMember() method', () => {
+    expect(teamMember.getMember()).toBe('Employee');
+});
